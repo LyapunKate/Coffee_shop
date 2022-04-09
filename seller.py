1a265d801ef99e0dc44997f8d03c30ccf1331787
LyapunKate <91290812+LyapunKate@users.noreply.github.com>
2022-04-09 12:57:19 +0300

    collected all seller's function in one work cycle
    Now the seller can start working by starting a shift, sell until his shift is over, then finish the shift.

--- seller.py
+++ seller.py
@@ -1,32 +1,78 @@
 from DataBase import Data
 
 class seller_options:
+
         def choose_option(self):
-            print('Выберете дальнейшее действие:\n 1. Начать смену\n 2. Продать\n 3. Завершить смену\n')
-            data = Data()
+            global end
+            end = 'begin'
             for i in range(1, 3):
+                print('Выберете дальнейшее действие:\n 1. Начать смену\n 2. Продать\n 3. Закончить смену\n')
                 activities = input()
                 match activities:
                     case 'Начать смену':
-                        print ('Введите фамилию\n')
-                        surname = input()
-                        print ('Введите дату и время начало\n')
-                        start_date = input()
-                        data.begin_work(surname, start_date)
+
+                        end = self.seller_begin_work()
+                        break
+
+
                     case 'Продать':
-                        print ('Введите название продукта\n')
-                        product = input()
-                        product_sel_id = data.select('id', 'Products', 'ProductName = {}'.format(product))
-                        print ('Введите количество\n')
-                        quantity = input()
-                        time = data.select_time()
-                        data.insert(product_sel_id, quantity, time)
+                        end = self.seller_sell()
+                        break
 
                     case 'Закончить смену':
-                        print ('Введите фамилию\n')
-                        surname = input()
-                        print ('Введите дату и время начало\n')
-                        end_date = input()
-                        data.end_work(surname, end_date)
+                        end = self.seller_end_work()
+                        break
+
                     case _:
                         print('Неверное действие. Попробуйте ещё раз')
+
+            return end
+
+        def seller_begin_work(self):
+            global end
+            data = Data()
+            print('Введите фамилию\n')
+            surname = input()
+            start_date = data.select_time()
+            data.begin_work(surname, start_date)
+            print('Выберете дальнейшее действие:\n 1. Продать\n 2. Закончить смену\n')
+            activities = input()
+            match activities:
+                case 'Продать':
+                    self.seller_sell()
+                case 'Закончить смену':
+                    end = self.seller_end_work()
+            return end
+
+        def seller_sell(self):
+            global end
+            data = Data()
+            print('Введите название продукта\n')
+            product = input()
+            product_sel_id = data.select('id', 'Products', 'ProductName = {}'.format(product))
+            print('Введите количество\n')
+            quantity = input()
+            time = data.select_time()
+            data.insert(product_sel_id, quantity, time)
+            print('Покупка успешно проведена\n')
+            print('Если хотите закончить введите END\n')
+            print('Если хотите продолжить введите C\n')
+            next = input()
+            match next:
+                case 'C':
+                    self.seller_sell()
+                case 'END':
+                    end = self.seller_end_work()
+            return end
+
+
+        def seller_end_work(self):
+            global end
+            data = Data()
+            print('Введите фамилию\n')
+            surname = input()
+            end_date = data.select_time()
+            data.end_work(surname, end_date)
+            print('Работа успешно завершена')
+            end = 'end'
+            return end
