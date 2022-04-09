97165b007d46f2347b4ba7de8e380aab240468b2
LyapunKate <91290812+LyapunKate@users.noreply.github.com>
2022-04-09 13:23:21 +0300

    add conecting into functions

--- DataBase.py
+++ DataBase.py
@@ -1,8 +1,9 @@
 import psycopg2 as psycopg2
 
 class Data:
-#pip3 install psycopg2
-  def connect(self):
+
+  def select(self, what, where, how):
+    global select_result
     con = psycopg2.connect(
       database='Coffee',
       user='postgres',
@@ -10,45 +11,72 @@ class Data:
       host='localhost',
       port=5432
     )
-    return con
-
-  def select(self, what, where, how):
-    cursor = self.connect.cursor()
+    cursor = con.cursor()
     cursor.execute("SELECT {} FROM {} WHERE {}".format(what, where, how))
 # Получаем результат сделанного запрос
     results = cursor.fetchall()
-    print(results)
-    self.connect.close()
+    for row in results:
+      select_result = row[0]
+    cursor.close()
+    return select_result
 
   def begin_work(self, who, start_date):
-    cursor = self.connect.cursor()
-    cursor.execute("INSERT INTO work(surname, start_date) Values ({}, '{}')".format(who, start_date))
+    con = psycopg2.connect(
+      database='Coffee',
+      user='postgres',
+      password='VarvaRa14',
+      host='localhost',
+      port=5432
+    )
+    cursor = con.cursor()
+    cursor.execute("INSERT INTO Work(surname, start_date) Values ({}, '{}')".format(who, start_date))
 # Получаем результат сделанного запро
-    self.connect.commit()
-    self.connect.close()
+    con.commit()
+    cursor.close()
 
   def end_work(self, who, end_date):
-    cursor = self.connect.cursor()
-    cursor.execute("INSERT INTO work(end_date) Values ('{}') where surname = {} and end_date IS NULL".format(end_date, who))
+    con = psycopg2.connect(
+      database='Coffee',
+      user='postgres',
+      password='VarvaRa14',
+      host='localhost',
+      port=5432
+    )
+    cursor = con.cursor()
+    cursor.execute("UPDATE Work SET end_date = '{}' where surname = {} and end_date IS NULL".format(end_date, who))
     # Получаем результат сделанного запро
-    self.connect.commit()
-    self.connect.close()
+    con.commit()
+    cursor.close()
 
   def insert(self, id, quantity, time):
-    cursor = self.connect.cursor()
-    cursor.execute("INSERT INTO purchases(ProductID, Quantity, DateTime) Values ({}, '{}', '{}')".format(id, quantity, time))
+    con = psycopg2.connect(
+      database='Coffee',
+      user='postgres',
+      password='VarvaRa14',
+      host='localhost',
+      port=5432
+    )
+    cursor = con.cursor()
+    cursor.execute("INSERT INTO purchases(ProductID, Quantity, DateTime) Values ({}, {}, '{}')".format(id, quantity, time))
     # Получаем результат сделанного запро
-    self.connect.commit()
-    self.connect.close()
+    con.commit()
+    cursor.close()
 
   def select_time(self):
-    cursor = self.connect.cursor()
+    global time_result
+    con = psycopg2.connect(
+      database='Coffee',
+      user='postgres',
+      password='VarvaRa14',
+      host='localhost',
+      port=5432
+    )
+    cursor = con.cursor()
     cursor.execute("SELECT NOW()")
 # Получаем результат сделанного запрос
     results = cursor.fetchall()
-    self.connect.close()
-    return results
+    for row in results:
+      time_result = row[0]
+    cursor.close()
+    return time_result
 
-
-# [('A Cor Do Som',), ('Aaron Copland & London Symphony Orchestra',), ('Aaron Goldberg',)]
-#
