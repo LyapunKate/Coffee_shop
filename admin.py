a26287e22a2e7cb3577d361632784a39477a68ec
Ioannsls <96343017+Ioannsls@users.noreply.github.com>
2022-05-14 00:40:51 +0300

    Update admin.py
    Добавлено завершение работы, добавлены локи действий на время взаимодействия с бд

--- admin.py
+++ admin.py
@@ -1,23 +1,32 @@
 from DataBase import Data
-
+import threading
+lock = threading.RLock()
 class admin_options:
     def choose_option(self):
-        print('Выберете дальнейшее действие:\n 1. Работники\n 2. Продажи\n 3. Склад\n')
-        for i in range(1, 4):
+        print('Выберете дальнейшее действие:\n 1. Работники\n 2. Продажи\n 3. Склад\n 4. Закончить работу\n')
+        for i in range(1, 5):
             activities = input()
             match activities:
                 case 'Работники':
                     self.workers()
+                    break
                 case 'Продажи':
                     self.purchases()
+                    break
                 case 'Склад':
                     self.quantity()
+                    break
+                case 'Закончить работу':
+                    self.admin_end_works()
+                    break
                 case _:
                     print('Неверное действие. Попробуйте ещё раз')
 
     def workers(self):
         print('')
+        self.choose_option()
     def purchases(self):
+
         global worker_sel_id, product_sel_id
         data = Data()
 
@@ -25,17 +34,28 @@ class admin_options:
         print('Кассир -- \n')
         worker = input()
         if (worker != 0):
-            worker_sel_id = data.select('id', 'Sellers', 'sellers.surname = {}'.format(worker))
+            with lock:
+                worker_sel_id = data.select('id', 'Sellers', 'sellers.surname = {}'.format(worker))
         print('Продукт -- \n')
         product = input()
         if (product != 0):
-            product_sel_id = data.select('id', 'Products', 'ProductName = {}'.format(product))
+            with lock:
+                product_sel_id = data.select('id', 'Products', 'ProductName = {}'.format(product))
         print('Начальная дата --\n')
         start_date = input()
 
         print('Конечная дата --\n')
         end_date = input()
-        data.select('*', 'Purchases', 'PearsonID = {} AND ProductID = {} '.format(worker_sel_id, product_sel_id))
+        with lock:
+            data.select('*', 'Purchases', 'PearsonID = {} AND ProductID = {} '.format(worker_sel_id, product_sel_id))
+
+        # возможно, тут должен быть вывод
+        self.choose_option()
+
+
     def quantity(self):
         print('')
-
+        self.choose_option()
+    def admin_end_works(self):
+        print('Работа успешно завершена')
+        return
